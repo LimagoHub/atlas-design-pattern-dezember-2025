8b2bb2ed21e3ab8bfe34859c011b3ef0b8b39ceb
Jo Wagner <jowagner@limago.de>
2025-12-01 15:51:31 +0100

    initial commit

--- Tag1_06TrafficLight/trafficlight/state/TrafficLightState.h
+++ Tag1_06TrafficLight/trafficlight/state/TrafficLightState.h
@@ -1,20 +1,19 @@
 //
-// Created by JoachimWagner on 01.12.2025.
+// Created by JoachimWagner on 25.12.2023.
 //
 
 #pragma once
-
 #include <string>
-
 namespace trafficlight::state {
 
     class TrafficLightState {
-    public:
-        virtual ~TrafficLightState() = default;
+            public:
+            virtual ~TrafficLightState() = default;
 
-        virtual std::string getColor() = 0;    // Business
-        virtual void nextColor() = 0; // Statuswechsel
+            virtual std::string getColor() = 0;    // Business
+            virtual void nextColor() = 0; // Statuswechsel
+
 
     };
 
-} // state
+}
